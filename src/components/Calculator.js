17d881376312b0f33a5956605c7bc2eb45d3f310
wuyepabdul <wuyepabdul@gmail.com>
2021-10-20 22:49:45 +0100

    Display result

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,11 +5,26 @@ import calculate from '../logic/calculate';
 
 const Calculator = () => {
   const [state, setState] = useState({ total: null, next: null, operation: null });
+
   const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
   const runCalculator = (btn) => {
     const newObj = calculate(state, btn);
     setState(newObj);
+  };
+
+  const displayResult = () => {
+    const { next, total, operation } = state;
+    const op = operation === '%' ? 'mod' : operation;
+    let result = '';
+    if (total) {
+      result = `${total} ${op || ''} ${next || ''}`;
+      return result;
+    } if (next) {
+      result = `${next} ${op || ''}`;
+      return result;
+    }
+    return 0;
   };
 
   const generateBtnClass = (className) => {
