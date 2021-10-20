6026148b00156f62444031944b1673e6a460b7c7
wuyepabdul <wuyepabdul@gmail.com>
2021-10-20 22:47:52 +0100

    Change components from class base to function base

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,50 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-export default class Calculator extends Component {
-  render() {
-    const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-    const iterator = 0;
-    const btnClass = (className) => {
-      // ;
-      if (className === '÷' || className === 'x' || className === '-' || className === '+' || className === '=') {
-        return 'orange';
-      } if (className === '0') { return 'light-grey zero'; }
-      return 'light-grey';
-    };
-    return (
-      <div className="calculator">
-        <div className="header">0</div>
-        {buttons.map((btn) => (
-          <div key={iterator + 1} className={btnClass(btn)}>
-            {' '}
-            {btn}
-            {' '}
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const Calculator = () => {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+  const runCalculator = (btn) => {
+    const newObj = calculate(state, btn);
+    setState(newObj);
+  };
+
+  const displayResult = () => {
+    const { next, total, operation } = state;
+    const op = operation === '%' ? 'mod' : operation;
+    let result = '';
+    if (total) {
+      result = `${total} ${op || ''} ${next || ''}`;
+      return result;
+    } if (next) {
+      result = `${next} ${op || ''}`;
+      return result;
+    }
+    return 0;
+  };
+  const generateBtnClass = (className) => {
+    if (className === '÷' || className === 'x' || className === '-'
+    || className === '+' || className === '=') {
+      return 'orange';
+    } if (className === '0') { return 'light-grey zero'; }
+    return 'light-grey';
+  };
+
+  return (
+    <div className="calculator">
+      <div className="header">{displayResult() }</div>
+      {buttons.map((btn) => (
+        <div key={btn} className={generateBtnClass(btn)} onClick={() => runCalculator(btn)}>
+          {' '}
+          {btn}
+          {' '}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Calculator;
