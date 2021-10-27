d96bc5b83eebf07b15ee8ad592357808012d87cc
wuyepabdul <wuyepabdul@gmail.com>
2021-10-27 21:27:01 +0100

    Style CalculatorPage component

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Calculator from '../components/Calculator';
 
 const CalculatorPage = () => (
-  <div>
+  <div className="calculator-container">
+    <h1> Let&apos;s do some math! </h1>
     <Calculator />
   </div>
 );
